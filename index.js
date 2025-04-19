1a58b5817c767171f41712f9534cb4f81f783906
Madson Santos <madsonsamsung@gmail.com>
2025-04-19 10:51:05 -0300

    Implements Gemini API

---
+++ index.js
@@ -0,0 +1,35 @@
+import { config } from "dotenv";
+config();
+
+import { GoogleGenAI } from "@google/genai";
+
+async function main() {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
+  const config = {
+    responseMimeType: "text/plain",
+  };
+  const model = "gemini-2.0-flash";
+  const contents = [
+    {
+      role: "user",
+      parts: [
+        {
+          text: `O que vc pode fazer?`,
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  for await (const chunk of response) {
+    console.log(chunk.text);
+  }
+}
+
+main();
